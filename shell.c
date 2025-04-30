2c797be9e7d200de240159bd8000c665cda1d442
EmilMajid <emilinpoctu@gmail.com>
2025-04-30 17:50:35 +0000

    add

--- shell.c
+++ shell.c
@@ -6,56 +6,74 @@
 #include <sys/wait.h>
 #include "main.h"
 
+
+
+
+
+void set_argv(char *line, char ***argv)
+{
+	char *line_copy = NULL;
+	char *arg;
+	unsigned long argc;
+	unsigned long i = 0;
+
+	line_copy = strdup(line);
+
+	arg = strtok(line_copy, " \n");
+	while (arg != NULL)
+	{
+		arg = strtok(NULL, " \n");
+		argc++;
+	}
+	free(line_copy);
+
+	*argv = malloc(sizeof(char *) * (argc + 1));
+	arg = strtok(line, " \n");
+	for (i = 0; i < argc; i++)
+	{
+		(*argv)[i] = arg;
+		arg = strtok(NULL, " \n");
+	}
+	(*argv)[i] = NULL;
+}
+
+
+
+
+
+
+
+
+
 int main(void)
 {
 	char *line = NULL;
-	char *line_copy = NULL;
 	char **argv;
-	char *arg;
 	unsigned long line_len = 0;
 	long line_read = 0;
-	unsigned long argc = 0;
-	unsigned long i = 0;
 	unsigned int pid;
 	int status;
 
-	while (1)
-	{
 		line_read = getline(&line, &line_len, stdin);
-		if (line_read == -1)
-		{
-			break;
-		}
+		set_argv(line, &argv);
 
-		line_copy = strdup(line);
-		arg = strtok(line_copy, " \n");
-		while (arg != NULL)
-		{
-			arg = strtok(NULL, " \n");
-			argc++;
+		for (int i = 0; argv[i] != NULL; i++) {
+			printf("%s\n", argv[i]);
 		}
-		free(line_copy);
-
-		argv = malloc(sizeof(char *) * (argc + 1));
-		arg = strtok(line, " \n");
-		for (i = 0; i < argc; i++)
-		{
-			argv[i] = arg;
-			arg = strtok(NULL, " \n");
-		}
-		argv[argc] = NULL;
-
+	
+		/*
 		pid = fork();
 
 		if (pid == 0)
 		{
-			execve(argv[0], argv, environ);
+			execve(argv[0], argv, NULL);
 		}
 		else
 		{
 			wait(&status);
+			free(line);
+			free(argv);
 		}
-
-	}
+*/
 	return (0);
 }
